5e09b5ea51ed437d0973ed2a11084af46f72e014
FernandoAdrogue <chinoadrogue@gmail.com>
2023-05-18 11:20:13 -0300

    homework 01-Node complete

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -2,7 +2,22 @@ const process = require('process');
 const { Z_ASCII } = require('zlib');
 const commands = require('./commands/index.js');
 
-function bash() {}
+function bash() {
+   process.stdout.write('prompt > ')
+   process.stdin.on("data",(data)=>{
+      const param = String(data).split(" ")
+      let args = param.slice(1).join(" ").replace(/(\r\n|\n|\r)/gm,"")
+      let cmd = param[0].trim()
+      Object.keys(commands).includes(cmd) ?
+         commands[cmd](print, args) :
+         print(`command not found: ${cmd}`)
+   })
+}
+
+const print = (output)=>{
+   process.stdout.write(output)
+   process.stdout.write('\nprompt > ')
+} 
 
 bash();
 module.exports = {
