79cd0c297c563bb323789fd5963e956664222c81
FernandoAdrogue <chinoadrogue@gmail.com>
2023-05-30 20:28:42 -0300

    homework 04 avanced promises chap5  4 off 9

--- 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
+++ 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
@@ -10,6 +10,42 @@ function $Promise(executor){
     executor(this._internalResolve.bind(this),this._internalReject.bind(this))
 }
 
+$Promise.resolve = function(value){
+    if (value instanceof $Promise){
+        return value
+        } else {
+        return new $Promise(function(promiseResolve){
+            promiseResolve(value)
+        })
+    }
+}
+
+$Promise.all = function(promiseGroups){
+    if(!(promiseGroups instanceof Array)){
+        throw TypeError ("Is not a Array")
+    }
+    if(promiseGroups.every(element=>!(element instanceof $Promise))){
+        return this.resolve(promiseGroups)
+    }else
+        if(promiseGroups.every(element=> element instanceof $Promise)){
+                   
+           return this.resolve(promiseGroups.map(
+                element=>element._value))
+    }
+    else{
+        const valueGroups = []
+        while(promiseGroups.length){
+            const element = promiseGroups.shift()
+            if(!(element instanceof $Promise)){
+                valueGroups.push(element)
+            } 
+            else{
+                valueGroups.push(element._value)
+            }
+        }
+        this.resolve(valueGroups)
+    }
+}
 
 $Promise.prototype._internalResolve = function(data){
     if(this._state === 'pending'){
@@ -28,10 +64,10 @@ $Promise.prototype._internalReject = function(reason){
 }
 
 $Promise.prototype.then = function (successCb,errorCb) {
-
+    
     if (typeof successCb !== "function") successCb  = false
     if (typeof errorCb !== "function") errorCb  = false
-
+    
     const downstreamPromise = new $Promise(()=>{})
     this._handlerGroups.push(
         {
@@ -39,17 +75,17 @@ $Promise.prototype.then = function (successCb,errorCb) {
             errorCb,
             downstreamPromise
         }
-    )
-
-    if(this._state !== "pending") this._callHandlers(this._value)
-
-    return downstreamPromise
-}
-
-$Promise.prototype.catch = function (errorCb){
-    return this.then(null,errorCb)
-}
-
+        )
+        
+        if(this._state !== "pending") this._callHandlers(this._value)
+        
+        return downstreamPromise
+    }
+    
+    $Promise.prototype.catch = function (errorCb){
+        return this.then(null,errorCb)
+    }
+    
 $Promise.prototype._callHandlers = function (value){
     while (this._handlerGroups.length) {
         const handler = this._handlerGroups.shift()
@@ -61,9 +97,9 @@ $Promise.prototype._callHandlers = function (value){
                         return result.then(
                             (data) => {handler.downstreamPromise._internalResolve(data)},
                             (reason) => {handler.downstreamPromise._internalReject(reason)}
-                        )
-                    } else {
-                        handler.downstreamPromise._internalResolve(result)
+                            )
+                        } else {
+                            handler.downstreamPromise._internalResolve(result)
                     }
                 } catch (error) {
                     return handler.downstreamPromise._internalReject(error)
@@ -80,24 +116,21 @@ $Promise.prototype._callHandlers = function (value){
                         return result.then(
                             (data) => {handler.downstreamPromise._internalResolve(data)},
                             (reason) => {handler.downstreamPromise._internalReject(reason)}
-                        )
-                    } else {
-                        handler.downstreamPromise._internalResolve(result)
-                    }   
-                } catch (error) {
-                    return handler.downstreamPromise._internalReject(error)
+                            )
+                        } else {
+                            handler.downstreamPromise._internalResolve(result)
+                        }   
+                    } catch (error) {
+                        return handler.downstreamPromise._internalReject(error)
+                    }
+                }  else {
+                    return handler.downstreamPromise._internalReject(value)
                 }
-            }  else {
-                return handler.downstreamPromise._internalReject(value)
             }
         }
     }
-}
 
-
-
-
-module.exports = $Promise;
+    module.exports = $Promise;
 /*-------------------------------------------------------
 El spec fue diseñado para funcionar con Test'Em, por lo tanto no necesitamos
 realmente usar module.exports. Pero aquí está para referencia:
